7f9ead3cc207f8a2fc2635380de1c7573119184a
LeoAnderssonKTH <132525577+LeoAnderssonKTH@users.noreply.github.com>
2024-04-16 16:35:33 +0200

    fixed router to calendar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Nav from './presenters/NavPresenter.jsx';
 import Header from './presenters/HeaderPresenter.jsx';
 import Footer from './presenters/FooterPresenter.jsx';
 import Home from './presenters/HomePresenter.jsx';
-//import MyCalendar from './presenters/MyCalendarPresenter.jsx';
+import MyCalendar from './presenters/MyCalendarPresenter.jsx';
 import RoomFavoritesList from './presenters/FavoritesPresenter.jsx';
 import {  createHashRouter,  RouterProvider } from "react-router-dom";
 import { observer } from "mobx-react-lite";
@@ -33,10 +33,10 @@ export default observer(function App(props) {
         path: "/home",
         element: <Home model={model}/>,
       },
-      /*{
+      {
         path: "/myCalendar",
-        element: <Home model={model}/>,
-      },*/
+        element: <MyCalendar model={model}/>,
+      },
       {
         path: "/roomFavoritesList",
         element: <Home model={model}/>,
