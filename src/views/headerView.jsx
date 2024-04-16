301b9a4157e55c6f1bec99010884144688d93368
anwestb <anwestb@kth.se>
2024-04-16 15:33:51 +0200

    header and footer fix
    -header and footer split and moved from index into presenter view combos and is now working

---
+++ src/views/headerView.jsx
@@ -0,0 +1,17 @@
+export default function FooterView(props) {
+    return (
+    <div>
+      <header class="header">
+        <h1>
+          <span class="part-one-header">Meeting </span>
+          <span class="part-two-header">Planner</span>
+        </h1>{" "}
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/115/115902.png"
+          alt="Hemsidans logga"
+        />
+      </header>
+    </div>
+    );
+  }
+  
