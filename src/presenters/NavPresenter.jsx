7f99a5d3469caf3f69f75c5ffe23d4f505b742b3
Seemamian <93984512+Seemamian@users.noreply.github.com>
2024-04-16 23:01:12 +0200

    NavBar clean up

--- src/presenters/NavPresenter.jsx
+++ src/presenters/NavPresenter.jsx
@@ -1,57 +1,60 @@
+import React from "react";
 import NavView from "../views/navView.jsx";
 import { observer } from "mobx-react-lite";
 
-export default function navViewPresenter(props) {
-  function handleroomListClicked() {
-    props.model.roomListDone.done = true;
-    props.model.getInfoOfArray(props.model.mediaroomList);
-  }
-  function handleSearchQueryACB(query) {
-    props.model.setSearchQuery(query);
-  }
-  function handleSearchButtonACB() {
-    props.model.doSearch();
-  }
-  function handleLogInOutACB() {
-    props.model.logInOrOutWithGoogle();
-  }
-  function handleMoreSearchACB() {
-    props.model.doMoreSearch();
-  }
+const NavViewPresenter = observer(({ model }) => {
+  const handleRoomListClicked = () => {
+    model.roomListDone.done = true;
+    model.getInfoOfArray(model.mediaroomList);
+  };
 
-  function getMoreSearchAmount() {
-    let moreSearch;
+  const handleSearchQueryACB = (query) => {
+    model.setSearchQuery(query);
+  };
 
-    if (props.model.getMaxResults() != null)
-      moreSearch = props.model.getMaxResults() + " sidor återstår att visas";
-    else moreSearch = "";
-    return moreSearch;
-  }
-  function checkSearchAvailable() {
-    if (
-      !props.model.searchResultsPromiseState?.data ||
-      props.model.getMaxResults() <= 0
-    )
-      return true;
-    else return false;
-  }
+  const handleSearchButtonACB = () => {
+    model.doSearch();
+  };
+
+  const handleLogInOutACB = () => {
+    model.logInOrOutWithGoogle();
+  };
+
+  const handleMoreSearchACB = () => {
+    model.doMoreSearch();
+  };
+
+  const getMoreSearchAmount = () => {
+    if (model.getMaxResults() != null) {
+      return `${model.getMaxResults()} sidor återstår att visas`;
+    } else {
+      return "";
+    }
+  };
+
+  const checkSearchAvailable = () => {
+    return !model.searchResultsPromiseState?.data || model.getMaxResults() <= 0;
+  };
+
   return (
     <NavView
       onSearchQuery={handleSearchQueryACB}
       onSearchButton={handleSearchButtonACB}
       onLogInLogOut={handleLogInOutACB}
       onMoreSearch={handleMoreSearchACB}
-      isLoggedIn={props.model.userState.isLoggedIn}
-      user={props.model.userState.user}
-      query={props.model.searchParams.q}
+      isLoggedIn={model.userState.isLoggedIn}
+      user={model.userState.user}
+      query={model.searchParams.q}
       moreSearchAmount={getMoreSearchAmount()}
       searchAvailable={checkSearchAvailable()}
-      hasSearched={props.model.searchResultsPromiseState.data}
-      onHandleroomListClicked={handleroomListClicked}
-      rooms={props.model.typeList}
-      navOpen={props.model.isnavOpen}
-      SearchIconActive={props.model.isSearchIconActive}
-      doneSearch={props.model.searchDone}
+      hasSearched={model.searchResultsPromiseState.data}
+      onHandleroomListClicked={handleRoomListClicked}
+      rooms={model.typeList}
+      navOpen={model.isnavOpen}
+      SearchIconActive={model.isSearchIconActive}
+      doneSearch={model.searchDone}
     />
   );
-}
+});
+
+export default NavViewPresenter;
--- src/views/navView.jsx
+++ src/views/navView.jsx
@@ -1,66 +1,83 @@
+import React from "react";
+
 export default function NavView(props) {
-  function IconActive() {
-    props.SearchIconActive.active = true;
-
-    setTimeout(() => {
-      props.SearchIconActive.active = false;
-    }, 100);
-  }
-
-  function routeToHomeACB(event) {
-    event.preventDefault();
-    window.location.hash = "#/home";
-  }
-
-  function routeToMyCalendarACB(event) {
-    event.preventDefault();
-    window.location.hash = "#/myCalendar";
-  }
-
-  function routeToRoomFavoritesListACB(event) {
-    event.preventDefault();
-    window.location.hash = "#/roomFavoritesList";
-  }
-
-  function handleSearchACB(event) {
+  const handleSearchACB = (event) => {
     props.onSearchQuery(event.target.value);
-  }
+  };
 
-  function handleSearchDoingACB(event) {
+  const handleSearchDoingACB = (event) => {
     if (event.key === "Enter") {
       props.onSearchButton();
       window.location.hash = "#/search";
     }
-  }
+  };
 
-  function handleResetButtonClick() {
+  const handleResetButtonClick = () => {
     window.location.hash = "#/";
-  }
+  };
 
-  function handleLogInOutACB(event) {
-    props.onLogInLogOut();
-  }
-
-  function handleMoreSearchACB(event) {
+  const handleMoreSearchACB = () => {
     if (window.location.hash !== "#/search") {
       window.location.hash = "#/search";
     }
     props.onMoreSearch();
-  }
+  };
 
-  function onSearchIconClick() {
+  const onSearchIconClick = () => {
     props.onSearchButton();
     window.location.hash = "#/search";
-  }
+  };
 
-  function onHandleroomListClickedACB() {
+  const onHandleroomListClickedACB = () => {
     window.location.hash = "#/roomList";
     props.onHandleroomListClicked();
-  }
+  };
 
-  function onHandleSearchClickedACB() {
-    //window.location.hash = "#/search";
-  }
+  const renderMoreSearch = () => {
+    if (props.hasSearched) {
+      return (
+        <>
+          <li>
+            <button onClick={handleMoreSearchACB} disabled={!props.searchAvailable}>
+              <h5>Visa ytterligare en sida</h5>
+            </button>
+          </li>
+          <li>
+            <p>Totalt {props.hasSearched.totalItems} resultat</p>
+            <p>{props.moreSearchAmount}</p>
+          </li>
+        </>
+      );
+    }
+  };
+
+  const renderLoggedInContent = () => {
+    if (props.isLoggedIn) {
+      return (
+        <>
+          <li>
+            <p>Inloggad som: {props.user.displayName}, {props.user.email}</p>
+          </li>
+          <li>
+            <img src={props.user.photoURL} alt="Användarens profil" />
+          </li>
+          <li>
+            <button onClick={props.onLogInLogOut}>
+              <h5>Logga ut</h5>
+            </button>
+          </li>
+        </>
+      );
+    } else {
+      return (
+        <li>
+          <button onClick={props.onLogInLogOut}>
+            <h5>Logga in med Google</h5>
+          </button>
+        </li>
+      );
+    }
+  };
 
   return (
     <nav className="nav">
@@ -79,22 +96,7 @@ export default function NavView(props) {
           />
         </li>
         <li onClick={onSearchIconClick} className="search-icon search"></li>
-        {props.hasSearched && (
-          <>
-            <li>
-              <button
-                onClick={handleMoreSearchACB}
-                disabled={!props.searchAvailable}
-              >
-                <h5>Visa ytterligare en sida</h5>
-              </button>
-            </li>
-            <li>
-              <p>Totalt {props.hasSearched.totalItems} resultat</p>
-              <p>{props.moreSearchAmount}</p>
-            </li>
-          </>
-        )}
+        {renderMoreSearch()}
       </ul>
 
       <ul className="nav-section" id="links-nav-section">
@@ -107,23 +109,23 @@ export default function NavView(props) {
           </button>
         </li>
         <li>
-          <button onClick={routeToHomeACB}>
+          <button onClick={() => window.location.hash = "#/home"}>
             <h4>Home</h4>
           </button>
         </li>
         <li>
-          <button onClick={routeToMyCalendarACB}>
+          <button onClick={() => window.location.hash = "#/myCalendar"}>
             <h4>My Calendar</h4>
           </button>
         </li>
         <li>
-          <button onClick={routeToRoomFavoritesListACB}>
+          <button onClick={() => window.location.hash = "#/roomFavoritesList"}>
             <h4>Room Favorites List</h4>
           </button>
         </li>
         {props.doneSearch.done && (
           <li>
-            <button onClick={onHandleSearchClickedACB}>
+            <button onClick={() => window.location.hash = "#/search"}>
               <h4>Söksida</h4>
             </button>
           </li>
@@ -141,29 +143,7 @@ export default function NavView(props) {
         <li>
           <h3>Inloggning</h3>
         </li>
-        {props.isLoggedIn ? (
-          <>
-            <li>
-              <p>
-                Inloggad som: {props.user.displayName}, {props.user.email}
-              </p>
-            </li>
-            <li>
-              <img src={props.user.photoURL} alt="Användarens profil" />
-            </li>
-            <li>
-              <button onClick={handleLogInOutACB}>
-                <h5>Logga ut</h5>
-              </button>
-            </li>
-          </>
-        ) : (
-          <li>
-            <button onClick={handleLogInOutACB}>
-              <h5>Logga in med Google</h5>
-            </button>
-          </li>
-        )}
+        {renderLoggedInContent()}
       </ul>
     </nav>
   );
