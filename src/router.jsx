7e6b8b50d68de7c84abb88d480276ab76de4059a
Carl-Anton Grandelius <cagra@kth.se>
2024-04-16 13:16:18 +0200

    small fix

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router"; //Ska vara i REACT!
 import FavouritesPresenter from "/src/presenters/favouritesPresenter.jsx";
-import MainContentPresenter from "/src/presenters/HomePresenter";
+import HomePresenter from "/src/presenters/HomePresenter";
 import mainWelcomeView from "/src/views/mainWelcomeView.jsx";
 
 export default function makeRouter(model) {
@@ -13,7 +13,7 @@ export default function makeRouter(model) {
       },
       {
         path: "/search",
-        component: <MainContentPresenter model={model}></MainContentPresenter>,
+        component: <HomePresenter model={model}></HomePresenter>,
       },
       {
         path: "/favourites",
