5af120b1b4e50fcc70f351fce17fa2d84547baec
LeoAnderssonKTH <132525577+LeoAnderssonKTH@users.noreply.github.com>
2024-04-16 16:42:30 +0200

    fixed router for calendar after merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./presenters/NavPresenter.jsx";
 import Header from "./presenters/HeaderPresenter.jsx";
 import Footer from "./presenters/FooterPresenter.jsx";
 import Home from "./presenters/HomePresenter.jsx";
+import MyCalendar from './presenters/MyCalendarPresenter.jsx';
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 
@@ -16,27 +17,17 @@ export default observer(function App(props) {
       <Footer model={props.model} />
     </div>
   );
-    
-  // Routes!
-  function makeRouter(model) {
-    return createHashRouter([
-      {
-        path: "/",
-        element: <Welcome model={model}/>,
-      },
-      {
-        path: "/home",
-        element: <Home model={model}/>,
-      },
-      /*{
-        path: "/myCalendar",
-        element: <Home model={model}/>,
-      },*/
-      {
-        path: "/roomFavoritesList",
-        element: <Home model={model}/>,
-      }
-    ])
-  }
-  
 });
+
+function makeRouter(model) {
+  return createHashRouter([
+    { path: "/", element: <Welcome model={model} /> },
+    { path: "/home", element: <Home model={model} /> },
+    {
+      path: "/myCalendar",
+      element: <MyCalendar model={model}/>,
+    },
+    { path: "/roomFavoritesList", element: <Home model={model} /> },
+  ]);
+}
+
