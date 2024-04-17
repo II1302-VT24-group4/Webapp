9843f8ff5266449cb28242b9bf9add073dec9401
LeoAnderssonKTH <132525577+LeoAnderssonKTH@users.noreply.github.com>
2024-04-17 15:39:51 +0200

    when a new user is created its saved in the database

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -7,13 +7,18 @@ import {
   signOut,
 } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc, updateDoc, getDocs, getDoc} from "firebase/firestore";
+
 
 // initialize the firebase app
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 // initialize the database
-const db = getFirestore()
+export const db = getFirestore();
+
+// Adding documents test
+// Add a new document in collection "cities"
+
 
 // exports for google auth
 const auth = getAuth(app);
@@ -21,25 +26,30 @@ const provider = new GoogleAuthProvider();
 export {auth, provider};
 
 export function googleSignInOut(model) {
-  console.log(model.user)
-  console.log(model.isLoggedIn)
+  //console.log(model.user)
+  //console.log(model.isLoggedIn)
   signInWithPopup(auth,new GoogleAuthProvider())
   .then(successfulLoginACB)
 
   
 
   function successfulLoginACB(fbUserChunk){
-    //console.log(model.userState.isLoggedIn)
-
-      
       //model.user_ID = fbUserChunk.user.uid;
       //model.userState = true;
+      console.log(fbUserChunk)
 
       model.user = fbUserChunk.user.uid;
       model.isLoggedIn = true;
-      console.log(model)
+      //§console.log(model)
       window.location.hash="#/home";
       //console.log("Successful login! Welcome "+auth.currentUser.email);
+
+      //create a new user entry in database
+      
+      const userRef = doc(db, 'users', model.user);
+      setDoc(userRef, {email: fbUserChunk.user.email, RFID: 4321}, { merge: true });
+
+      
   }
 }
 
