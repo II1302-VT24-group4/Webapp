35522166faf009bff52808d83e3743706aa61e36
anwestb <anwestb@kth.se>
2024-04-17 10:52:57 +0200

    events can now be added
    -using prompts

--- src/views/myCalendarView.jsx
+++ src/views/myCalendarView.jsx
@@ -8,12 +8,62 @@ import interactionPlugin from '@fullcalendar/interaction';
 export default function MyCalendarView(props) {
   const calendarRef = useRef(null);
 
+  function createEvent(calendar, title, fullDay, startDate, endDate, date){
+    if(fullDay){
+      calendar.addEvent({
+        title: title,
+        allDay: fullDay, // Ensure the event is treated as a timed event
+        start: date
+      });
+    }
+    else{
+      calendar.addEvent({
+        title: title,
+        start: startDate,
+        end: endDate,
+        allDay: fullDay // Ensure the event is treated as a timed event
+      });
+    }
+
+    // Rerender the calendar to display the new event
+    calendar.render();
+  }
+
+  function convertTime(info, time){
+    var date = new Date(info.startStr);
+    var [hours, minutes] = time.split(':').map(Number);
+    date.setHours(hours, minutes);
+    return date;
+  }
+
   useEffect(() => {
     const calendarEl = calendarRef.current;
     const calendar = new Calendar(calendarEl, {  
-      dateClick: function() {alert('a day has been clicked!');},
       plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
-      initialView: 'dayGridMonth',
+      initialView: 'timeGridWeek',
+      selectable: true, // Enable selection
+      select: function(info) {
+        var title = prompt('Enter event title:');
+        const fullDayInput = prompt('Enter true if it goes on all day or false otherwise');
+        const fullDay = fullDayInput.toLowerCase() === 'true';
+        var startDate, endDate = null;
+        if(fullDay != true){
+          var startTime = prompt('Enter start time (e.g., 13:00):');
+          var endTime = prompt('Enter end time (e.g., 15:00):');
+          startDate = convertTime(info, startTime);
+          endDate = convertTime(info, endTime);
+        }
+        else{
+          var date = prompt('Enter date for the full day event (e.g., 2024-04-16):')
+        }
+
+        console.log("title " + title + ", start " + startTime + ", end " + endTime + ", allDay " + fullDay + ", date" + date)
+
+        if (title && ((startTime && endTime) || (fullDay && date))) {
+          createEvent(calendar, title, fullDay, startDate, endDate, date);
+        }
+      },
+      forceEventDuration: true, // Ensure events are displayed even without an end time
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
