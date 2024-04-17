bbb67c3e23df56c02790f3d46ef9444cdafcd62d
Carl-Anton Grandelius <cagra@kth.se>
2024-04-17 16:31:22 +0200

    Small fix

--- src/views/navView.jsx
+++ src/views/navView.jsx
@@ -7,7 +7,7 @@ export default function NavView(props) {
       {/* Search Section */}
       <ul className="nav-section" id="search-nav-section">
         <li>
-          <h3>Sök</h3>
+          <h3>Find room</h3>
         </li>
         <li>
           <input
@@ -15,7 +15,7 @@ export default function NavView(props) {
             value={props.query}
             onChange={(event) => props.onSearchQuery(event.target.value)}
             onKeyDown={(event) => props.onSearchButton(event)}
-            placeholder="Sök"
+            placeholder="Find Room"
             className="search-input"
           />
         </li>
@@ -33,34 +33,39 @@ export default function NavView(props) {
             <h4>Välkomstsida</h4>
           </button>
         </li>
-        <li>
-          <button onClick={() => window.location.hash = "#/home"}>
-            <h4>Home</h4>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => window.location.hash = "#/myCalendar"}>
-            <h4>My Calendar</h4>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => window.location.hash = "#/roomFavoritesList"}>
-            <h4>Room Favorites List</h4>
-          </button>
-        </li>
+
         {props.doneSearch.done && (
           <li>
-            <button onClick={() => window.location.hash = "#/search"}>
+            <button onClick={() => (window.location.hash = "#/search")}>
               <h4>Söksida</h4>
             </button>
           </li>
         )}
         {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onHandleroomListClicked}>
-              <h4>Favoriter</h4>
-            </button>
-          </li>
+          <>
+            <li>
+              <button onClick={props.onHandleroomListClicked}>
+                <h4>Favoriter</h4>
+              </button>
+            </li>
+            <li>
+              <button onClick={() => (window.location.hash = "#/home")}>
+                <h4>Home</h4>
+              </button>
+            </li>
+            <li>
+              <button onClick={() => (window.location.hash = "#/myCalendar")}>
+                <h4>My Calendar</h4>
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => (window.location.hash = "#/roomFavoritesList")}
+              >
+                <h4>Room Favorites List</h4>
+              </button>
+            </li>{" "}
+          </>
         )}
       </ul>
 
