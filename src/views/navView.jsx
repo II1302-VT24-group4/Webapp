66d5452ebfffb10100b4562dc8e5be28be75c9ac
Seemamian <93984512+Seemamian@users.noreply.github.com>
2024-04-28 12:35:48 +0200

    nav bar update horizontal

--- src/views/navView.jsx
+++ src/views/navView.jsx
@@ -2,47 +2,47 @@ import React from "react";
 
 export default function NavView(props) {
   return (
-    <nav class="nav">
+    <nav>
       {props.isLoggedIn && (
-        <ul class="nav-section" id="search-nav-section">
+        <ul>
           <li>
-            <h3>Find room</h3>
+            Find room
           </li>
-          <input
-            type="text"
-            value={props.query}
-            onChange={(event) => props.onSearchQuery(event.target.value)}
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                props.onSearchButton();
-                window.location.hash = "#/bookableRooms";
-              }
-            }}
-            placeholder="Find Room"
-            class="search-input"
-          />
+          <li>
+            <input
+              type="text"
+              value={props.query}
+              onChange={(event) => props.onSearchQuery(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  props.onSearchButton();
+                  window.location.hash = "#/bookableRooms";
+                }
+              }}
+              placeholder="Find Room"
+            />
+          </li>
         </ul>
       )}
-      <ul class="nav-section" id="links-nav-section">
+      <ul>
         <li>
-          <h3>Pages</h3>
+          Pages
         </li>
         <li>
           <button onClick={props.handleResetButtonClick}>
-            <h4>Welcome page</h4>
+            Welcome page
           </button>
         </li>
-
         {props.isLoggedIn && (
           <>
             <li>
               <button onClick={() => (window.location.hash = "#/home")}>
-                <h4>Home</h4>
+                Home
               </button>
             </li>
             <li>
               <button onClick={() => (window.location.hash = "#/myCalendar")}>
-                <h4>My Calendar</h4>
+                My Calendar
               </button>
             </li>
             <li>
@@ -52,7 +52,7 @@ export default function NavView(props) {
                   props.onSearchButton();
                 }}
               >
-                <h4>Bookable rooms</h4>
+                Bookable rooms
               </button>
             </li>
             <li>
@@ -62,7 +62,7 @@ export default function NavView(props) {
                   props.onRoomListClicked();
                 }}
               >
-                <h4>Room Favourites List</h4>
+                Room Favourites List
               </button>
             </li>
             <li>
@@ -71,16 +71,16 @@ export default function NavView(props) {
                   window.location.hash = "#/meeting";
                 }}
               >
-                <h4>Hold a meeting</h4>
+                Hold a meeting
               </button>
             </li>
           </>
         )}
       </ul>
       {/* Log-in Section */}
-      <ul class="nav-section" id="log-in-nav-section">
+      <ul>
         <li>
-          <h3>Log in</h3>
+          Log in
         </li>
         {props.renderLoggedInContent && props.renderLoggedInContent()}
       </ul>
