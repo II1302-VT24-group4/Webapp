276210b51c98bd371e13590dbd5563ba2adc07dd
anwestb <anwestb@kth.se>
2024-05-02 10:19:10 +0200

    updated MyCalendar for insert document

--- src/presenters/MyCalendarPresenter.jsx
+++ src/presenters/MyCalendarPresenter.jsx
@@ -3,30 +3,67 @@ import { observer } from "mobx-react-lite";
 
 export default observer(function MyCalendarPresenter(props) {    
 
-  const insertMeeting = async (event) => {
-    const meetings = await props.model.firebaseRead('users', props.model.userState.user, 'meetings');
-    const newID = meetings?.length || 0;
-
+  const insertMeetingDB = async (event) => {
+    const user = props.model.userState.user;
+    const room = event.room;
+    //Get ID of new meeting in user
+    const newID = Math.random().toString(36).substring(2) + Date.now().toString(36);
+    //Go through each attribute and add it to the newly created meeting in the DB
     for(const attribute of Object.keys(event)){
+      //The value of the given attribute
       const value = event[attribute];
-      props.model.firebaseInsert('users', props.model.userState.user, 'meetings', newID.toString(), attribute, value, true);
+      //Add to the users collection of meetings
+      if(attribute != "owner"){
+        props.model.firebaseInsert('users', user, 'meetings', newID, attribute, value, true);
+      }
+      //Add to the rooms collection of meetings
+      if(attribute != "room"){
+        props.model.firebaseInsert('rooms', room, 'meetings', newID, attribute, value, true);
+      }
+    }
+    //Add room owner to the meeting in the rooms db collection
+    props.model.firebaseInsert('rooms', room, 'meetings', newID, "owner", user, true);
+    return newID;
+  }
+
+  const updateMeetingDB = async (event) => {
+    const user = props.model.userState.user;
+    const meetingID = event.id;
+    const room = event.room;
+    for(const attribute of Object.keys(event)){
+      if(attribute != "room" && attribute != "id"){
+        const value = event[attribute];
+        props.model.firebaseInsert('rooms', room, 'meetings', meetingID, attribute, value, true);
+        props.model.firebaseInsert('users', user, 'meetings', meetingID, attribute, value, true);
+      }
     }
   }
 
-  const getDBMeetings = async () => {
-    while (props.model.userState.user === null) {
+  const deleteMeetingDB = async (event) => {
+    const user = props.model.userState.user;
+    const meetingID = event.id;
+    const room = event.room;
+    props.model.firebaseDelete('rooms', room, 'meetings', meetingID);
+    props.model.firebaseDelete('users', user, 'meetings', meetingID);
+  }
+
+  const getMeetingsDB = async () => {
+    const user = props.model.userState.user;
+    while (user === null) {
       // Wait for a short period before checking again
       await new Promise(resolve => setTimeout(resolve, 100));
     }
-    return await props.model.firebaseRead('users', props.model.userState.user, 'meetings');
+    return await props.model.firebaseRead('users', user, 'meetings');
   }
 
     return (
       <div>
         {<MyCalendarView
-          user = {props.model.userState}
-          addMeeting = {insertMeeting}
-          getMeetings = {getDBMeetings}
+          user={props.model.userState}
+          addMeeting={insertMeetingDB}
+          updateMeeting={updateMeetingDB}
+          deleteMeeting={deleteMeetingDB}
+          getMeetings={getMeetingsDB}
         />}
       </div>
     );
--- src/views/myCalendarView.jsx
+++ src/views/myCalendarView.jsx
@@ -10,57 +10,107 @@ export default function MyCalendarView(props) {
   const calendarRef = useRef(null);
   const [calendar, setCalendar] = useState(null);
   const [selectedInfo, setSelectedInfo] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(null);
   const [eventTitle, setEventTitle] = useState("");
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
-  const [allDay, setAllDay] = useState(false);
   const [file, setFile] = useState(null); // State to hold uploaded file
+  const [calendarInitialized, setCalendarInitialized] = useState(false); // New state to track calendar initialization
+
+  const isOverlapping = async (newEvent) => {
+    const events = await calendar.getEvents();
+    for (const oldEvent in events) {
+      if (newEvent.endDate > events[oldEvent].start && newEvent.startDate < events[oldEvent].end && newEvent.id != events[oldEvent].id) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   async function populateCalendar() {
-    const meetings = await props.getMeetings();
+    const meetings = await props.getMeetings(props.room);
     for (const meeting of meetings) {
       const title = meeting.title;
-      const startDate = meeting.startDate.toDate();
-      const endDate = meeting.endDate.toDate();
-      createEvent(title, startDate, endDate);
+      const startDate = meeting.startDate?.toDate();
+      const endDate = meeting.endDate?.toDate();
+      const id = meeting.id;
+      const owner = meeting.owner;
+      createEvent(title, startDate, endDate, owner, id);
     }
   }
 
-  function createEvent(title, startDate, endDate) {
+  function createEvent(title, startDate, endDate, owner, id){
     calendar.addEvent({
       title: title,
       start: startDate,
-      end: endDate
+      end: endDate,
+      id: id,
+      extendedProps: {
+        owner: owner
+      },
     });
     calendar.render();
   }
 
-  const handleSaveEvent = () => {
-    const startDate = convertTime(selectedInfo, startTime);
-    const endDate = convertTime(selectedInfo, endTime);
+  const handleCreateEvent = async () => {
+    const startDate = new Date(startTime);
+    const endDate = new Date(endTime);
+    const owner = props.user.user;
+    const eventData = {
+      title: eventTitle,
+      startDate: startDate,
+      endDate: endDate,
+      room: props.room,
+      owner: owner
+    };
+
+    if(await isOverlapping(eventData)){
+      alert("event is overlapping another event");
+    }
+    else{
+      const id = await props.addMeeting(eventData);
+
+      createEvent(
+        eventTitle,
+        startDate,
+        endDate,
+        owner,
+        id
+      );
+    }
+    setEventTitle("");
+    setStartTime("");
+    setEndTime("");
+    setSelectedInfo(null);
+  }
+
+  const handleUpdateEvent = async () => {
+    const startDate = new Date(startTime);
+    const endDate = new Date(endTime);
 
     const eventData = {
       title: eventTitle,
       startDate: startDate,
       endDate: endDate,
-      file: file // Include file in the event data
+      room: props.room,
+      id: selectedEvent.id,
+      file: file
     };
 
-    props.addMeeting(eventData);
+    if(await isOverlapping(eventData)){
+      alert("event is overlapping another event");
+    }
+    else{
+      props.updateMeeting(eventData);
 
-    createEvent(
-      eventTitle,
-      startDate,
-      endDate
-    );
-    setSelectedInfo(null); // Close the popup after saving the event
-  };
-
-  function convertTime(info, time) {
-    var date = new Date(info.startStr);
-    var [hours, minutes] = time.split(":").map(Number);
-    date.setHours(hours, minutes);
-    return date;
+      selectedEvent.setProp('title', eventTitle);
+      selectedEvent.setStart(startDate);
+      selectedEvent.setEnd(endDate);
+    }
+    setEventTitle("");
+    setStartTime("");
+    setEndTime("");
+    setSelectedEvent(null);
   }
 
   const handleFileChange = (e) => {
@@ -73,7 +123,10 @@ export default function MyCalendarView(props) {
     const newCalendar = new Calendar(calendarEl, {
       plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
       initialView: "timeGridWeek",
-      selectable: true,
+      allDaySlot: false,
+      slotMinTime: "06:00:00", // Show slots starting from 6 AM
+      slotMaxTime: "21:00:00", // Show slots until 9 PM
+      selectable: true, // Enable selection
       select: function (info) {
         if (!info.startStr.includes("T")) {
           info.startStr += "T13:00:00+02:00";
@@ -84,7 +137,10 @@ export default function MyCalendarView(props) {
         info.allDay = false;
         setSelectedInfo(info);
       },
-      forceEventDuration: true,
+      eventClick: function (event) {
+        setSelectedEvent(event.event);
+      },
+      forceEventDuration: true, // Ensure events are displayed even without an end time
       headerToolbar: {
         left: "prev,next today",
         center: "title",
@@ -93,6 +149,7 @@ export default function MyCalendarView(props) {
     });
     setCalendar(newCalendar);
     newCalendar.render();
+    setCalendarInitialized(true);
 
     // Cleanup function to destroy the calendar when the component unmounts
     return () => {
@@ -100,21 +157,36 @@ export default function MyCalendarView(props) {
     };
   }, []);
 
+  // Populate calendar after initialization
+  useEffect(() => {
+    if (calendarInitialized) {
+      populateCalendar();
+    }
+  }, [calendarInitialized]);
+
+  // Initialize startTime and endTime with the times from selectedInfo, if available, and set their constants for use when creating event
   useEffect(() => {
     if (selectedInfo) {
-      const startTimeStr = selectedInfo.startStr.split("T")[1].substring(0, 5);
+      const startTimeStr = new Date(selectedInfo.start.getTime() - selectedInfo.end.getTimezoneOffset() * 60000).toISOString().slice(0, 16);
       setStartTime(startTimeStr);
-
-      const endTimeStr = new Date(selectedInfo.startStr);
-      endTimeStr.setHours(endTimeStr.getHours() + 1);
-      setEndTime(
-        `${endTimeStr.getHours().toString().padStart(2, "0")}:${endTimeStr
-          .getMinutes()
-          .toString()
-          .padStart(2, "0")}`
-      );
+      
+      const endTimeStr = new Date(selectedInfo.end.getTime() - selectedInfo.end.getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+      setEndTime(endTimeStr);
     }
   }, [selectedInfo]);
+
+  // Initialize startTime, endTime and title with the times from selectedEvent, if available, and set their constants for use when updating event
+  useEffect(() => {
+    if (selectedEvent) {
+      const startTimeStr = new Date(selectedEvent.start.getTime() - selectedEvent.end.getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+      setStartTime(startTimeStr);
+      
+      const endTimeStr = new Date(selectedEvent.end.getTime() - selectedEvent.end.getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+      setEndTime(endTimeStr);
+
+      setEventTitle(selectedEvent.title);
+    }
+  }, [selectedEvent]);
 
   return (
     <div style={{ width: "98%", margin: "5" }}>
@@ -124,8 +196,7 @@ export default function MyCalendarView(props) {
           open={selectedInfo !== null}
           onClose={() => {
             setSelectedInfo(null);
-            setEventTitle("");
-            setAllDay(false);
+            setEventTitle(""); // Reset event title input
           }}
           modal
           closeOnDocumentClick
@@ -135,71 +206,152 @@ export default function MyCalendarView(props) {
               padding: "20px",
               background: "white",
               borderRadius: "5px",
+              textAlign: "right"
             }}
           >
-            <h2 style={{ marginBottom: "10px" }}>Create Event</h2>
-            <input
-              type="text"
-              placeholder="Event Title"
-              style={{ marginBottom: "10px", width: "100%" }}
-              value={eventTitle}
-              onChange={(e) => setEventTitle(e.target.value)}
-            />
-            <div style={{ marginBottom: "10px" }}>
-              <label htmlFor="startTime">Start Time:</label>
+            <h2 style={{ marginBottom: "20px" }}>Create Event</h2>
+            <div>
+              <label htmlFor="title">Title:&nbsp;&nbsp;&nbsp;&nbsp;</label>
               <input
-                type="time"
+                office="text"
+                placeholder="Event Title"
+                style={{ marginBottom: "10px", width: "164px"}}
+                value={eventTitle}
+                onChange={(e) => setEventTitle(e.target.value)}
+              />
+            </div>
+            <div style={{ marginBottom: "10px" }}>
+              <label htmlFor="startTime">Start Time:&nbsp;</label>
+              <input
+                type="datetime-local"
+                office="time"
                 id="startTime"
                 style={{ marginLeft: "10px" }}
                 value={startTime}
-                onChange={(e) => setStartTime(e.target.value)}
-                disabled={allDay}
-              />
+                onChange={(e) => {
+                  const selectedHours = e.target.value.split(':')[0];
+                  setStartTime(`${selectedHours}:00`);
+                }}                
+                />
             </div>
             <div style={{ marginBottom: "10px" }}>
               <label htmlFor="endTime">End Time:</label>
               <input
-                type="time"
+                type="datetime-local"
+                office="time"
                 id="endTime"
                 style={{ marginLeft: "14px" }}
                 value={endTime}
-                onChange={(e) => setEndTime(e.target.value)}
-                disabled={allDay}
+                onChange={(e) => {
+                  const selectedHours = e.target.value.split(':')[0];
+                  setEndTime(`${selectedHours}:00`);
+                }}  
               />
             </div>
-          
-            <div style={{ marginBottom: "10px" }}>
-              <input
-                type="checkbox"
-                id="allDay"
-                checked={allDay}
-                onChange={(e) => setAllDay(e.target.checked)}
-              />
-              <label htmlFor="allDay" style={{ marginLeft: "5px" }}>
-                All Day
-              </label>
+            <div style={{marginTop: "20px"}}>
+              <button
+                style={{ marginRight: "80px", backgroundColor: "white" }}
+                onClick={() => setSelectedInfo(null)}
+              >
+                Cancel
+              </button>
+              <button
+                style={{ marginRight:"20px", backgroundColor: "white" }}
+                onClick={handleCreateEvent}
+              >
+                Save Event
+              </button>
             </div>
-            <div style={{ marginBottom: "10px" }}>
+          </div>
+        </Popup>
+      )}
+      {selectedEvent && (
+        <Popup
+        open={selectedEvent !== null}
+        onClose={() => {
+          setSelectedEvent(null);
+        }}
+        modal
+        closeOnDocumentClick
+      >
+        <div
+          style={{
+            padding: "20px",
+            background: "white",
+            borderRadius: "5px",
+            textAlign: "right"
+          }}
+        >
+          <h2 style={{ marginBottom: "20px" }}>Edit Event</h2>
+          <div>
+            <label htmlFor="title">Title:&nbsp;&nbsp;&nbsp;&nbsp;</label>
+            <input
+              type="text"
+              placeholder="Event Title"
+              style={{ marginBottom: "10px", width: "164px"}}
+              value={eventTitle}
+              onChange={(e) => setEventTitle(e.target.value)
+              }
+            />
+          </div>
+          <div style={{ marginBottom: "10px" }}>
+            <label htmlFor="startTime">Start Time:&nbsp;</label>
+            <input
+              type="datetime-local"
+              id="startTime"
+              style={{ marginLeft: "10px" }}
+              value={startTime}
+              onChange={(e) => {
+                const selectedHours = e.target.value.split(':')[0];
+                setStartTime(`${selectedHours}:00`);
+              }}  
+            />
+          </div>
+          <div style={{ marginBottom: "10px" }}>
+            <label htmlFor="endTime">End Time:</label>
+            <input
+              type="datetime-local"
+              id="endTime"
+              style={{ marginLeft: "14px" }}
+              value={endTime}
+              onChange={(e) => {
+                const selectedHours = e.target.value.split(':')[0];
+                setEndTime(`${selectedHours}:00`);
+              }}
+            />
+          </div>
+          <div style={{ marginBottom: "10px" }}>
               <input
                 type="file"
                 id="file"
                 onChange={handleFileChange}
               />
             </div>
+          <div style={{marginTop: "20px"}}>
             <button
-              style={{ marginRight: "10px", backgroundColor: "white" }}
-              onClick={() => setSelectedInfo(null)}
+              style={{ marginRight: "35px", backgroundColor: "white" }}
+              onClick={() => {
+                setSelectedEvent(null);
+              }}
             >
               Cancel
             </button>
             <button
-              style={{ backgroundColor: "white" }}
-              onClick={handleSaveEvent}
+              style={{ marginRight: "35px", backgroundColor: "white" }}
+              //not sure if we want to be able to remove events in personal calendar
+              //onClick={handleRemoveEvent}
             >
-              Save Event
+              Remove
+            </button>
+            <button
+              style={{ marginRight: "10px", backgroundColor: "white" }}
+              onClick={handleUpdateEvent}
+            >
+              Confirm
             </button>
           </div>
-        </Popup>
+        </div>
+      </Popup>
       )}
     </div>
   );
